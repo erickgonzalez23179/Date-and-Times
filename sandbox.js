7ff60333fa58489a680155fddad2f57d9ef64ddb
Erick <egonzalez23179@columbushs.com>
2021-02-17 12:23:30 -0500

    done

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,21 @@
+
+const before = new Date(`February 1 2021 8:30:50`);
 const now = new Date();
-console.log(now)
-console.log(typeof now)
 
-console.log(`getFullYear:`, now.getFullYear())
-console.log(`getMonth:`, now.getMonth())
-console.log(`getDate:`, now.getDate())
-console.log(`getDay:`, now.getDay())
-console.log(`getHours:`, now.getHours())
-console.log(`getMinutes:`, now.getMinutes())
-console.log(`getSeconds:`, now.getSeconds())
+// console.log(now.getTime(), before.getTime())
 
-console.log(`timestamp`, now.getTime())
-console.log(now.toDateString())
-console.log(now.toTimeString())
-console.log(now.toLocaleString())
+const diff = now.getTime() - before.getTime()
+console.log(diff)
+
+const mins = Math.round(diff/ 1000 / 60)
+const hours = Math.round(mins/ 60)
+const days = Math.round(hours/ 24)
+
+console.log(mins)
+console.log(hours)
+console.log(days)
+
+console.log(`the blog was written ${days} ago.`)
+
+const timestamp = 1675938474990;
+console.log(new Date(timestamp));
