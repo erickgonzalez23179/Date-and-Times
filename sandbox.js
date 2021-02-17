ae1125cdc65ae1fabefa2c5c48a74de4acecaa97
Erick <egonzalez23179@columbushs.com>
2021-02-17 12:46:41 -0500

    done with section and lesson

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,16 @@
-const clock = document.querySelector('.clock');
+const now = new Date();
 
-const tick = () => {
+// console.log(dateFns.isToday(now))
 
-  const now = new Date();
-  
-  const h = now.getHours();
-  const m = now.getMinutes();
-  const s = now.getSeconds();
 
-  const html = `
-    <span>${h}</span> :
-    <span>${m}</span> :
-    <span>${s}</span>
-  `;
+// format
 
-  clock.innerHTML = html;
+console.log(dateFns.format(now, `YYYY`));
+console.log(dateFns.format(now, `MMMM`));
+console.log(dateFns.format(now, `dddd`));
+console.log(dateFns.format(now, `Do`));
+console.log(dateFns.format(now, `dddd Do MMMM YYYY`));
 
-};
+const before = new Date(`February 1 2019 12:00:00`)
 
-setInterval(tick, 1000);
+console.log(dateFns.distanceInWords(now, before, {addSuffix: true}))
